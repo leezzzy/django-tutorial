eed71042a5853ba854543ef94c16ad747f221ea9
leez <hnslzy@gmail.com>
2017-10-16 17:32:56 +0900

    첫 번째 장고 앱 작성하기, part 1(2)

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'as2ny@ldhh%w1h!j4zn2e&-+&f(hfu9cmsbnan0o-30e95p@0b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
